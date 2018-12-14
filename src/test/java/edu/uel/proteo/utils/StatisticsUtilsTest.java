6883b0eee1d87371c236b43a4289e53a42506be9
Fernando Gicci Hernandes <fgicci@me.com>
2018-12-14 19:37:22 +0000

    develop: added unit tests for Statistics Utils.

---
+++ src/test/java/edu/uel/proteo/utils/StatisticsUtilsTest.java
@@ -0,0 +1,38 @@
+package edu.uel.proteo.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StatisticsUtilsTest {
+
+	@Test
+	public void shouldTest_EuclidianDistance() {
+		double expect = 10;
+		assertEquals(expect, StatisticsUtils.euclidianDistance(10, 20), 0);
+	}
+	
+	@Test
+	public void shouldTest_EuclidianDistanceSameDimension() {
+		int expect = 11;
+		double[] a = { 5.0, 10.0 };
+		double[] b = { 10.0, 20.0 };
+		assertEquals(expect, (int) StatisticsUtils.euclidianDistance(a, b), 0);
+	}
+	
+	@Test
+	public void shouldTest_EuclidianDistanceWithADimension() {
+		int expect = 11;
+		double[] a = { 5.0, 10.0 };
+		double[] b = { 10.0, 20.0, 30.0 };
+		assertEquals(expect, (int) StatisticsUtils.euclidianDistance(a, b), 0);
+	}
+	
+	@Test
+	public void shouldTest_EuclidianDistanceWithBDimension() {
+		int expect = 11;
+		double[] a = { 5.0, 10.0, 20.0 };
+		double[] b = { 10.0, 20.0 };
+		assertEquals(expect, (int) StatisticsUtils.euclidianDistance(a, b), 0);
+	}
+}
